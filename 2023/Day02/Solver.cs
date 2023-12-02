ea1528ae1391f9f4bc00cf9d110805b56ca38570
Horizon0156 <stefan.wilkes@gmail.com>
2023-12-02 15:25:14 +0100

    Day 02

---
+++ 2023/Day02/Solver.cs
@@ -0,0 +1,52 @@
+namespace AdventOfCode.Y2023.Day02;
+
+[Problem("Cube Conundrum", 2023, 2)]
+internal class Solver : ISolver
+{
+    public Solution Solve(string input)
+    {
+        var games = input.SplitLines().Select(Game.Parse);
+
+        return new (
+            games.Where(g => g.IsPossible).Sum(g => g.Id),
+            games.Sum(g => g.Power));
+    }
+
+    private sealed record Game(int Id, bool IsPossible, int Power)
+    {
+        public static Game Parse(string game)
+        {
+            var gameParts = game.Split(":");
+            var rounds = gameParts[1].Split(";").Select(Round.Parse);
+            
+            return new (
+                int.Parse(gameParts[0].Replace("Game ", string.Empty)),
+                rounds.All(r => r.IsPossible),
+                rounds.Max(r => r.Red) * rounds.Max(r => r.Green) * rounds.Max(r => r.Blue)
+            );
+        }
+    }
+
+    private sealed record Round(int Red, int Green, int Blue)
+    {
+        public bool IsPossible => Red <= 12 && Green <= 13 && Blue <= 14;
+
+        public static Round Parse(string round)
+        {
+            var red = 0;
+            var green = 0;
+            var blue = 0;
+            var roundParts = round.Split(",");
+            foreach (var roundPart in roundParts)
+            {
+                if (roundPart.Contains("red"))
+                    red += int.Parse(roundPart.Replace(" red", string.Empty));
+                if (roundPart.Contains("green"))
+                    green += int.Parse(roundPart.Replace(" green", string.Empty));
+                if (roundPart.Contains("blue"))
+                    blue += int.Parse(roundPart.Replace(" blue", string.Empty));
+            }
+            return new (red, green, blue);
+        }
+    }
+}
